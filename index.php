26ea06acea0b932268073c2931f8d3224e6e611f
Łukasz Waszczuk <lukaszw1939@gmail.com>
2020-10-31 09:07:23 +0100

    Edycja css i poprawka w kodzie

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <body>
 	<div id="wrapper">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-8">2</div>
-			<div class="col-xs-6 col-md-4">1</div>
+			<div class="col-xs-12 col-sm-6 col-md-8">1</div>
+			<div class="col-xs-6 col-md-4">2</div>
 		</div>
 	</div>
 </body>
