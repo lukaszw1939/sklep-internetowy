62aa8e104f58ebcd6b2e29ff8fd759f701c85b6b
Łukasz Waszczuk <lukaszw1939@gmail.com>
2020-10-31 09:00:47 +0100

    dodanie style.css do kodu i stworzenie pliku style.css

--- head.php
+++ head.php
@@ -5,6 +5,7 @@
 
 		<link rel="stylesheet" href="css/bootstrap.css" />
 		<link rel="stylesheet" href="css/jquery-ui.css" />
+		<link rel="stylesheet" href="css/style.css" />		
 
 		<script src="js/jquery.js"></script>
 		<script src="js/jquery-ui.js"></script>
