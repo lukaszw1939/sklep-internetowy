2863738f492211ec9bedc91566a3a5f6b0982f21
Łukasz Waszczuk <lukaszw1939@gmail.com>
2020-10-31 09:31:04 +0100

    dodawanie jumbotron

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <body>
 	<div id="wrapper">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-8">1</div>
+			<div class="col-xs-12 col-sm-6 col-md-8">
+
+			<div class="jumbotron">
+  				<h1>Sklep Internetowy - LukaszenkoShop</h1>
+  				<p>Dodaj produkt, którego pragniesz się pozbyć. Pomożemy Ci sprzedać go szybko i za dobrą cenę</p>
+  				<p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+			</div>
+
+			</div>
 			<div class="col-xs-6 col-md-4">2</div>
 		</div>
 	</div>
