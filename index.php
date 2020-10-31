09d5e831cbc7f4f146dd933f480ef50c86ba3e0f
Łukasz Waszczuk <lukaszw1939@gmail.com>
2020-10-31 08:56:04 +0100

    Test bootstrap

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php include("head.php") ?>
 
 <body>
-
+	<div id="wrapper">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-8">2</div>
+			<div class="col-xs-6 col-md-4">1</div>
+		</div>
+	</div>
 </body>
 </html>
 
